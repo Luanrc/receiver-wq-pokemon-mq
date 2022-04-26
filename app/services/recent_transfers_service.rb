f34f66397d67efc29a60dede362c5b461f9974a7
Luan <luan.rodrigues.c@gmail.com>
2022-04-26 16:09:02 -0300

    fix: solve receiver service channel problem

--- app/services/recent_transfers_service.rb
+++ app/services/recent_transfers_service.rb
@@ -1,24 +1,21 @@
 class RecentTransfersService
   def initialize
     @connection = Bunny.new(hostname: 'rabbitmq')
+    @channel = connection.create_channel
   end
 
   def send_back(message)
     
-    exchange = channel.fanout("world_trader_pong", durable: true)
+    exchange = @channel.fanout("world_trader_pong", durable: true)
     
     puts '--------------enviou os dados para o pong-----------------------------'
     puts "Dados enviados para o ping: #{message}"
     exchange.publish(message.to_json)
 
-    # @connection.close
+    @connection.close
   end
 
   private
-
-  def channel
-    connection.create_channel
-  end
 
   def connection
     @connection.start
